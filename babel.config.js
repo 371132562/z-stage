80cce4fd6c04d379f170aee14f25b6d3672127b6
zhaolinxing <371132562@qq.com>
2019-09-25 17:30:06 +0800

    babel配置

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,14 @@ module.exports = {
     presets: ['@vue/app'],
     plugins: [
         [
-            /*element-ui按需引入*/
+            /* element-ui 按需引入*/
             'component',
             {
                 libraryName: 'element-ui',
                 styleLibraryName: 'theme-chalk'
             }
         ],
-        /*ant-design-vue按需引入*/
+        /* ant-design-vue 按需引入*/
         ['import', { libraryName: 'ant-design-vue', libraryDirectory: 'es', style: true }]
     ]
 }
