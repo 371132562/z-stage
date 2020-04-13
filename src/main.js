61d12abd33c1943ef1cf2fff540528c044ce68e6
zhaolinxing <371132562@qq.com>
2020-04-13 21:10:09 +0800

    重新创建项目

--- src/main.js
+++ src/main.js
@@ -1,19 +1,13 @@
-//框架
 import Vue from 'vue'
-import store from '@/store'
-
-//样式，js，插件
-import '@/plugins'
-
-//最后引入路由，App，防止自定义样式被覆盖
-
-import router from '@/router'
-import App from '@/App.vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-    router,
-    store,
-    render: h => h(App)
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -4,26 +4,24 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: Home
-    },
-    {
-        path: '/about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
-    }
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
 ]
 
 const router = new VueRouter({
-    routes
+  routes
 })
-router.beforeEach((to, from, next) => {
-    next()
-})
+
 export default router
